dbd559e93cb4217622ea61a541ad2080698de08b
Kyle Olivo <kyle@kyleolivo.com>
2017-06-30 15:24:29 -0700

    Add dummy app for initial pipeline setup

---
+++ palindrome_test.go
@@ -0,0 +1,18 @@
+package word
+
+import "testing"
+
+func TestPalindrome(t *testing.T) {
+	if !IsPalindrome("detartrated") {
+		t.Error(`IsPalindrome("detartrated") = false`)
+	}
+	if !IsPalindrome("kayak") {
+		t.Error(`IsPalindrome("kayak") = false`)
+	}
+}
+
+func TestNonPalindrome(t *testing.T) {
+	if IsPalindrome("palindrome") {
+		t.Error(`IsPalindrome("palindrome") = true`)
+	}
+}
