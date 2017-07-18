829ff2e1fbcffa3775dddf00cae486ddb8bbfb0e
Isabelle Carter <isabellenataliecarter@gmail.com>
2017-07-18 13:05:59 -0700

    Removing the timestamp prefix for log messages
    * also changed the argument in logger from location to prefix

--- logging/logger.go
+++ logging/logger.go
@@ -7,17 +7,19 @@ import (
 
 type ILogger interface {
 	Warn(msg string, tokens ...interface{})
-	Die(location, msg string, tokens ...interface{})
+	Die(prefix, msg string, tokens ...interface{})
 }
 
 type DefaultLogger struct {
 	ILogger
 }
 
-func (l *DefaultLogger) Die(location, msg string, tokens ...interface{}) {
-	log.Fatalf("FATAL ERROR (%v): %v\n", location, fmt.Sprintf(msg, tokens...))
+func (l *DefaultLogger) Die(prefix, msg string, tokens ...interface{}) {
+	log.SetFlags(0)
+	log.Fatalf("ERROR %v: %v\n", prefix, fmt.Sprintf(msg, tokens...))
 }
 
 func (l *DefaultLogger) Warn(msg string, tokens ...interface{}) {
+	log.SetFlags(0)
 	log.Println("WARNING:", fmt.Sprintf(msg, tokens...))
 }
