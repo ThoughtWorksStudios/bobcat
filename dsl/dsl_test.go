4cefa4c87d030b5531836bec5ce65b90f81fbfc9
Isabelle Carter <isabellenataliecarter@gmail.com>
2017-07-21 16:12:22 -0700

    Changing generate statement syntax
    * From: generate Entity(x)
    * TO: generate(x, Entity)

--- dsl/dsl_test.go
+++ dsl/dsl_test.go
@@ -50,8 +50,8 @@ func tableSpecForReservedWords() map[string]string {
 	for _, kw := range keyWords {
 		result[fmt.Sprintf(`def %s`, kw)] = fmt.Sprintf(`Illegal identifier: %q is a reserved word`, kw)
 		result[fmt.Sprintf(`def t { %s string }`, kw)] = fmt.Sprintf(`Illegal identifier: %q is a reserved word`, kw)
-		result[fmt.Sprintf(`generate %s`, kw)] = fmt.Sprintf(`Illegal identifier: %q is a reserved word`, kw)
-		result[fmt.Sprintf(`generate %s(3)`, kw)] = fmt.Sprintf(`Illegal identifier: %q is a reserved word`, kw)
+		result[fmt.Sprintf(`generate(3, %s)`, kw)] = fmt.Sprintf(`Illegal identifier: %q is a reserved word`, kw)
+		result[fmt.Sprintf(`generate(1, %s)`, kw)] = fmt.Sprintf(`Illegal identifier: %q is a reserved word`, kw)
 	}
 	return result
 }
@@ -87,33 +87,33 @@ func TestParsesChildEntity(t *testing.T) {
 }
 
 func TestParsesBasicGenerationStatement(t *testing.T) {
-	args := NodeSet{Node{Kind: "literal-int", Value: 1, Ref: NewLocation("", 1, 15, 14)}}
+	args := NodeSet{Node{Kind: "literal-int", Value: 1, Ref: NewLocation("", 1, 10, 9)}}
 	genBird := testGenEntity("Bird", NewLocation("", 1, 1, 0), NodeSet{}, args)
 	testRoot := testRootNode(NodeSet{genBird})
-	actual, err := Parse("", []byte("generate Bird(1)"))
+	actual, err := Parse("", []byte("generate(1, Bird)"))
 	AssertNil(t, err, "Didn't expect to get an error: %v", err)
 	AssertEqual(t, testRoot.String(), actual.(Node).String())
 }
 
 func TestCanParseMultipleGenerationStatements(t *testing.T) {
-	arg := Node{Kind: "literal-int", Value: 1, Ref: NewLocation("", 1, 15, 14)}
+	arg := Node{Kind: "literal-int", Value: 1, Ref: NewLocation("", 1, 10, 9)}
 	genBird := testGenEntity("Bird", NewLocation("", 1, 1, 0), NodeSet{}, NodeSet{arg})
-	arg.Ref = NewLocation("", 2, 16, 32)
-	bird2Gen := testGenEntity("Bird2", NewLocation("", 2, 1, 17), NodeSet{}, NodeSet{arg})
+	arg.Ref = NewLocation("", 2, 10, 27)
+	bird2Gen := testGenEntity("Bird2", NewLocation("", 2, 1, 18), NodeSet{}, NodeSet{arg})
 	bird2Gen.Name = "Bird2"
 	testRoot := testRootNode(NodeSet{genBird, bird2Gen})
-	actual, err := Parse("", []byte("generate Bird(1)\ngenerate Bird2(1)"))
+	actual, err := Parse("", []byte("generate(1, Bird)\ngenerate(1, Bird2)"))
 	AssertNil(t, err, "Didn't expect to get an error: %v", err)
 	AssertEqual(t, testRoot.String(), actual.(Node).String())
 }
 
 func TestCanOverrideFieldInGenerateStatement(t *testing.T) {
-	arg := Node{Kind: "literal-int", Value: 1, Ref: NewLocation("", 1, 15, 14)}
+	arg := Node{Kind: "literal-int", Value: 1, Ref: NewLocation("", 1, 10, 9)}
 	value := Node{Kind: "literal-string", Value: "birdie", Ref: NewLocation("", 1, 25, 24)}
 	field := testEntityField("name", NewLocation("", 1, 20, 19), value, nil)
 	genBird := testGenEntity("Bird", NewLocation("", 1, 1, 0), NodeSet{field}, NodeSet{arg})
 	testRoot := testRootNode(NodeSet{genBird})
-	actual, err := Parse("", []byte("generate Bird(1) { name \"birdie\" }"))
+	actual, err := Parse("", []byte("generate(1, Bird { name \"birdie\" })"))
 	AssertNil(t, err, "Didn't expect to get an error: %v", err)
 	AssertEqual(t, testRoot.String(), actual.(Node).String())
 }
@@ -126,20 +126,20 @@ func TestCanOverrideMultipleFieldsInGenerateStatement(t *testing.T) {
 	arg2 := Node{Kind: "literal-int", Value: 2, Ref: NewLocation("", 1, 49, 48)}
 	field2 := testEntityField("age", NewLocation("", 1, 35, 34), value2, NodeSet{arg1, arg2})
 
-	arg := Node{Kind: "literal-int", Value: 1, Ref: NewLocation("", 1, 15, 14)}
+	arg := Node{Kind: "literal-int", Value: 1, Ref: NewLocation("", 1, 10, 9)}
 	genBird := testGenEntity("Bird", NewLocation("", 1, 1, 0), NodeSet{field1, field2}, NodeSet{arg})
 	testRoot := testRootNode(NodeSet{genBird})
-	actual, err := Parse("", []byte("generate Bird(1) { name \"birdie\", age integer(1,2) }"))
+	actual, err := Parse("", []byte("generate(1, Bird { name \"birdie\", age integer(1,2) })"))
 	AssertNil(t, err, "Didn't expect to get an error: %v", err)
 	AssertEqual(t, testRoot.String(), actual.(Node).String())
 }
 
 func TestParsedBothBasicEntityAndGenerationStatement(t *testing.T) {
-	args := NodeSet{Node{Kind: "literal-int", Value: 1, Ref: NewLocation("", 2, 15, 26)}}
+	args := NodeSet{Node{Kind: "literal-int", Value: 1, Ref: NewLocation("", 2, 10, 21)}}
 	genBird := testGenEntity("Bird", NewLocation("", 2, 1, 12), NodeSet{}, args)
 	bird := testEntity("Bird", NewLocation("", 1, 1, 0), NodeSet{})
 	testRoot := testRootNode(NodeSet{bird, genBird})
-	actual, err := Parse("", []byte("def Bird {}\ngenerate Bird(1)"))
+	actual, err := Parse("", []byte("def Bird {}\ngenerate(1, Bird)"))
 	AssertNil(t, err, "Didn't expect to get an error: %v", err)
 	AssertEqual(t, testRoot.String(), actual.(Node).String())
 }
@@ -227,8 +227,8 @@ func TestReservedRulesRestrictions(t *testing.T) {
 // this may become obsolete once we start supporting nested entities and other value declarations
 func TestShouldGiveErrorForUnknownFieldTypes(t *testing.T) {
 	specs := map[string]string{
-		"generate(1) t { e eek }": `no match found, expected: "date", "decimal", "def", "dict", "false", "generate", "integer", "null", "string", "true", [ \t\r\n] or [a-z_]i`,
-		"def t { e blah }":        `no match found, expected: "-", "0", "\"", "\\0", "date", "decimal", "dict", "false", "integer", "null", "string", "t"i, "true", [ \t\r\n], [0-9] or [1-9]`,
+		"generate(1, t { e eek })": `no match found, expected: "-", "0", "\"", "\\0", "date", "decimal", "dict", "false", "integer", "null", "string", "t"i, "true", [ \t\r\n], [0-9] or [1-9]`,
+		"def t { e blah }":         `no match found, expected: "-", "0", "\"", "\\0", "date", "decimal", "dict", "false", "integer", "null", "string", "t"i, "true", [ \t\r\n], [0-9] or [1-9]`,
 	}
 
 	for spec, expectedErrMessage := range specs {
@@ -238,7 +238,7 @@ func TestShouldGiveErrorForUnknownFieldTypes(t *testing.T) {
 }
 
 func TestShouldGiveErrorWhenNoCountIsGivenToGenerate(t *testing.T) {
-	expectedErrMessage := `no match found, expected: "(", [ \t\r\n] or [a-z0-9_]i`
+	expectedErrMessage := `no match found, expected: "(" or [ \t\r\n]`
 	_, err := Parse("", []byte("generate Blah"))
 	ExpectsError(t, expectedErrMessage, removeLocationInfo(err))
 }
@@ -263,12 +263,12 @@ func TestFieldListWithoutCommas(t *testing.T) {
 
 func TestEntityNameMustBeAlphaNumericAndStartWithALetter(t *testing.T) {
 	specs := map[string]string{
-		"generate(1) 4": `no match found, expected: "date", "decimal", "def", "dict", "false", "generate", "integer", "null", "string", "true", [ \t\r\n] or [a-z_]i`,
-		"def 4 { }":     `no match found, expected: "date", "decimal", "def", "dict", "false", "generate", "integer", "null", "string", "true", [ \t\r\n] or [a-z_]i`,
-		"def $eek { }":  `no match found, expected: "date", "decimal", "def", "dict", "false", "generate", "integer", "null", "string", "true", [ \t\r\n] or [a-z_]i`,
-		"generate $eek": `no match found, expected: "date", "decimal", "def", "dict", "false", "generate", "integer", "null", "string", "true", [ \t\r\n] or [a-z_]i`,
-		"generate eek$": `no match found, expected: "(", [ \t\r\n] or [a-z0-9_]i`,
-		"def e$ek { }":  `no match found, expected: ":", "{", [ \t\r\n] or [a-z0-9_]i`,
+		"generate(1, 4)":    `no match found, expected: "date", "decimal", "def", "dict", "false", "generate", "integer", "null", "string", "true", [ \t\r\n] or [a-z_]i`,
+		"def 4 { }":         `no match found, expected: "date", "decimal", "def", "dict", "false", "generate", "integer", "null", "string", "true", [ \t\r\n] or [a-z_]i`,
+		"def $eek { }":      `no match found, expected: "date", "decimal", "def", "dict", "false", "generate", "integer", "null", "string", "true", [ \t\r\n] or [a-z_]i`,
+		"generate(1, $eek)": `no match found, expected: "date", "decimal", "def", "dict", "false", "generate", "integer", "null", "string", "true", [ \t\r\n] or [a-z_]i`,
+		"generate(1, eek$)": `no match found, expected: ")", "{", [ \t\r\n] or [a-z0-9_]i`,
+		"def e$ek { }":      `no match found, expected: ":", "{", [ \t\r\n] or [a-z0-9_]i`,
 	}
 
 	for spec, expectedErrMessage := range specs {
