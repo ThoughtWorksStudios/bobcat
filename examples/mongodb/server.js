f5b901ed62416d8086bd11d1c9da91c33b6f5609
Isabelle Carter <isabellenataliecarter@gmail.com>
2017-09-05 11:20:46 -0700

    Updating mongodb example

--- examples/mongodb/server.js
+++ examples/mongodb/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+var fs = require('fs')
 
 var db
 
@@ -19,7 +20,7 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  db.collection('quotes').find().toArray((err, result) => {
+  db.collection('flat_quotes').find().toArray((err, result) => {
     if (err) return console.log(err)
     users = db.collection('users').find().toArray((err, r) => {
     res.render('index.ejs', {quotes: result, nested: false, users: r, findAuthor: findAuthor})
@@ -32,6 +33,29 @@ app.get('/nested', (req, res) => {
     if (err) return console.log(err)
     res.render('index.ejs', {quotes: result, nested: true})
   })
+})
+
+app.get('/import/:file', (req, res) => {
+  file = 'bobcat/output/' + req.params['file']
+  fs.readFile(file, 'utf8', function (err, data) {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    obj = JSON.parse(data);
+    var collections = Object.keys(obj);
+    if (collections['0'] === '0') {
+      collection = req.params['file'].split('.')[0]
+        console.log(collection)
+      db.collection(collection).insertMany(obj)
+    } else {
+      for(i=0; i<collections.length; i++) {
+        collection = collections[i]
+        db.collection(collection).insertMany(obj[collection]);
+      }
+    }
+  });
+  res.send("done importing json data from " + file);
 })
 
 var findAuthor = function(quote, authors) {
